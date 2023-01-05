6d014d3ef92b5aaab0ce627b91e015c82fe6ae5d
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-05 12:06:17 +0100

    + Add a base for user page

--- src/app/userpage/app.userpage.ts
+++ src/app/userpage/app.userpage.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'user-page',
   templateUrl: './app.userpage.html',
   styleUrls: ['./app.userpage.css'],
 })
-export class UserPage {}
+export class UserPage {
+  constructor(private AppComponent: AppComponent, private router: Router) {}
+
+  getUserNickname = this.AppComponent.userNickname;
+}
