15c32b8895327ff7ba0928c79f0219fd76f5bf11
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-07 11:56:08 +0100

    1 file modified

--- src/app/deleteStudentPage/app.delete.student.ts
+++ src/app/deleteStudentPage/app.delete.student.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 
+import { LocalStorage } from '../localStorage';
+
 @Component({
   selector: 'delete-student-page',
   templateUrl: './app.delete.student.html',
   styleUrls: ['./app.delete.student.css'],
 })
 export class DeleteStudentPage {
+  constructor(private localStorage: LocalStorage) {}
+
   firstNameValue: string = "";
   lastNameValue: string = "";
 
-  deleteStudent: void {
-    
+  deleteStudent(): void {
+    for (var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      var value = localStorage.getItem(key).split(",");
+      if (key.length == 1) {
+        if (value[0] == this.firstNameValue && value[1] == this.lastNameValue) {
+          this.localStorage.removeData(key);
+        }
+      }    
+    }
   }
 }
