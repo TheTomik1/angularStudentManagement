204cff2a4d0056ebb21731740f47d8f8a7980665
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-08 16:54:56 +0100

    3 files modified

--- src/app/studentManagePage/app.student.manage.ts
+++ src/app/studentManagePage/app.student.manage.ts
@@ -9,7 +9,9 @@ import { LocalStorage } from '../localStorage';
   styleUrls: ['./app.student.manage.css'],
 })
 export class StudentManagePage {
-  constructor(private localStorage: LocalStorage) {}
+  constructor(private localStorage: LocalStorage, private router: Router) {}
+
+  errorMessage: string = "";
 
   firstNameValue: string = '';
   lastNameValue: string = '';
@@ -25,8 +27,13 @@ export class StudentManagePage {
   verifyFields(): boolean {
     if (this.firstNameValue == "" || this.lastNameValue == "" || this.classValue == "" || this.ageValue == 0 || this.birthdayValue == "" || this.fieldValue == "" || this.genderValue == "" ||
     this.percentageValue == 0 || this.disabledValue == "" || this.rewardsValue == "") {
-      return false
+      this.errorMessage = "Make sure that all the fields are filled with data and that the rewards are indeed split by a dot.";
+      return false;
     } else {
+      if (!this.rewardsValue.includes(".")) {
+        this.errorMessage = "Make sure that all the fields are filled with data and that the rewards are indeed split by a dot.";
+        return false;
+      }
       return true;
     }
   }
@@ -53,14 +60,14 @@ export class StudentManagePage {
     let currentMinutes = currentTime.getMinutes();
     let currentSeconds = currentTime.getSeconds();
     let currentTimeFormatted = `${currentDay}:${currentMonth}:${currentYear} ${currentHours}:${currentMinutes}:${currentSeconds}`;
-    console.log(currentTimeFormatted)
-
     let mergeData: string = `${this.firstNameValue},${this.lastNameValue},${this.classValue},${this.ageValue},${this.birthdayValue},${this.fieldValue},${this.genderValue},${this.percentageValue},${this.disabledValue},${this.rewardsValue},${currentTimeFormatted}`;
 
     if (this.verifyFields() == true) {
-      console.log(mergeData)
-      this.localStorage.setData(getHighestId.toString(), mergeData)
-      console.log(this.localStorage.getData("6"));
+      this.localStorage.setData(getHighestId.toString(), mergeData);
+      this.router.navigate(["viewstudentdetail"], {queryParams: { id: getHighestId.toString() }});
+      this.errorMessage = "";
+    } else {
+
     }
   }
 }
