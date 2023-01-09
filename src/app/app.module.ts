990a6fd5f34b4fe08344629ba567bfe4ebf5ecbd
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-09 19:34:35 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,10 @@ import { LogoutPage } from './logOutPage/app.logout';
 import { UserPage } from './userPage/app.userpage';
 import { StudentViewPage } from './studentsViewPage/app.students.view';
 import { StudentDetailPage } from './studentDetailPage/app.student.detail';
-import { StudentManagePage } from './studentManagePage/app.student.manage';
 import { StudentDeletePage } from './studentDeletePage/app.student.delete';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, StudentManagePage],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [AppComponent],
   declarations: [AppComponent, LogoutPage, UserPage, StudentViewPage, StudentDetailPage, StudentDeletePage],
   bootstrap: [AppComponent],
