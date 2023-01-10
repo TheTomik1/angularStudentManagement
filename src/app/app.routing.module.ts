24d9fdc0f8dde2f60d00c68a7f01c539e6d8fab0
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-10 12:57:06 +0100

    1 file modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 
 import { LogoutPage } from './logOutPage/app.logout';
 import { UserPage } from './userPage/app.userpage';
@@ -8,9 +8,23 @@ import { StudentDetailPage } from './studentDetailPage/app.student.detail';
 import { StudentAddPage } from './studentAddPage/app.student.add';
 import { StudentDeletePage } from './studentDeletePage/app.student.delete';
 
+import { AppComponent } from './app.component';
+
+@Injectable()
+class LoggedInGuard implements CanActivate {
+  constructor(private appComponent: AppComponent) {}
+
+  canActivate() {
+      if (this.appComponent.userStatus != "Currently you are not logged in") {
+        alert("You don't have permission to view this page"); (4)
+        return false;
+      }
+  }
+}
+
 const routes: Routes = [
   { path: 'logout', component: LogoutPage },
-  { path: 'user/:id', component: UserPage },
+  { path: 'user/:id', component: UserPage, canActivate: [LoggedInGuard] },
   { path: 'viewstudents', component: StudentViewPage },
   { path: 'viewstudentdetail', component: StudentDetailPage },
   { path: 'deletestudent', component: StudentDeletePage },
