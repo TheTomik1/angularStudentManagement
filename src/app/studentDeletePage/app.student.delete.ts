55bd10b244bc5cdeadf376d0b131431a54339785
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-01-10 18:37:10 +0100

    3 files modified

--- src/app/studentDeletePage/app.student.delete.ts
+++ src/app/studentDeletePage/app.student.delete.ts
@@ -13,31 +13,22 @@ export class StudentDeletePage {
 
   firstNameValue: string = "";
   lastNameValue: string = "";
-  statusMessage: string = "";
-  yesNoCardEnabled: boolean = false;
 
-  handleCard(): void {
-    this.yesNoCardEnabled = !this.yesNoCardEnabled;
-  }
-
-  findStudent(): string {
+  findStudent(): void {
     for (var i = 0; i < localStorage.length; i++) {
       var key = localStorage.key(i);
       var value = localStorage.getItem(key).split(",");
       if (key.length == 1) {
         if (value[0] == this.firstNameValue.trim() && value[1] == this.lastNameValue.trim()) {
-          this.handleCard();
-          return key;
+          this.deleteStudent(key);
         }
       }    
     }
-    this.statusMessage = "This student has not been found in the system. Be sure to check your spelling.";
-    return "notfound";
+    alert("This student has not been found in the system. Be sure to check your spelling.");
   }
 
   deleteStudent(key: string): void {
       this.localStorage.removeData(key);
-      this.statusMessage = "";
       this.router.navigate(["viewstudents"]);
   }
 }
